f80314f75fc46a84c926b567448aa4a9d47c0b3c
Michael R. Joseph <unceus@users.noreply.github.com>
2022-01-13 14:32:37 -0800

    update volume script

--- .scripts/volume.sh
+++ .scripts/volume.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-mixer=$(amixer get Master | grep 'Left: Playback')
+grep='Left\:\ Playback'
+
+mixer=$(amixer get Master | grep 'Left\:\ Playback')
 #echo $mixer | cut -d ' ' -f 4
 #echo $mixer | cut -d ' ' -f 6
 
-echo $mixer | cut -d ' ' -f 5
+muted=$(amixer get Master | grep 'Left\:\ Playback' | cut -d ' ' -f 9)
+if [ "$muted" == "[off]" ]; then
+	echo "[--]"
+	else
+	echo $mixer | cut -d ' ' -f 5
+fi
